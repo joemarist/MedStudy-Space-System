a56ce3fb4005a7c7b1adefe5242b0b66232c3974
cjdoong <doongd97@gmail.com>
2025-03-26 19:49:36 +0800

    TRYING MY BEST EVEN ITS HARD
    HAHAHAHAHHA

--- Admin/js/dashboard.js
+++ Admin/js/dashboard.js
@@ -1,11 +1,37 @@
 function titlechanges(num){
     if(num == 1){
         document.getElementById("titlename").textContent = "Dashboard";
+
+        document.getElementById("dashboard").style.display = "block";
+        document.getElementById("rooms").style.display="none";
+        document.getElementById("Booking").style.display ="none";
+        document.getElementById("reports").style.display="none";
+
+
     }else if(num == 2){
         document.getElementById("titlename").textContent = "Room";
+
+
+        document.getElementById("dashboard").style.display = "none";
+        document.getElementById("rooms").style.display="block";
+        document.getElementById("Booking").style.display ="none";
+        document.getElementById("reports").style.display="none";
+
     }else if(num == 3){
         document.getElementById("titlename").textContent = "Booking";
+
+        document.getElementById("dashboard").style.display = "none";
+        document.getElementById("rooms").style.display="none";
+        document.getElementById("Booking").style.display ="block";
+        document.getElementById("reports").style.display="none";
+
     }else{
         document.getElementById("titlename").textContent = "Reports";
+
+        document.getElementById("dashboard").style.display = "none";
+        document.getElementById("rooms").style.display="none";
+        document.getElementById("Booking").style.display ="none";
+        document.getElementById("reports").style.display="block";
+
     }
 }
